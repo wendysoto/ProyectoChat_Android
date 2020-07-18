c6b37b926af42995d8c0e638404ab0977a0b6d9b
AndresDav7 <andres.alvear01@epn.edu.ec>
2020-07-18 16:17:45 -0500

    Fragments Usuarios, Toolbar

--- ChatApp/app/src/main/java/com/example/chatapp/MainActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/MainActivity.java
@@ -47,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Iniciar Sesión");
+
         auth = FirebaseAuth.getInstance();
 
         email = findViewById(R.id.email);
