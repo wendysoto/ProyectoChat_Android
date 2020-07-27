b8b082847268c3915a5fc8ce28234a1c9f4ad858
erickvargas10 <erick.vargas01@epn.edu.ec>
2020-07-27 00:09:19 -0500

    Interfaces , logos, spla, informe, conexión

--- ChatApp/app/src/main/java/com/example/chatapp/MainActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,98 +1,14 @@
-package com.example.chatapp;
+﻿package com.example.examen;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText email, password;
-    Button btn_login, btn_register;
-
-    FirebaseUser firebaseUser;
-    FirebaseAuth auth;
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        //Verifica si el usuario es null
-        if(firebaseUser != null){
-            Intent intent = new Intent(MainActivity.this, InitActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Iniciar Sesión");
-
-        auth = FirebaseAuth.getInstance();
-
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-        btn_login = findViewById(R.id.btn_login);
-        btn_register = findViewById(R.id.btnRegister);
-
-
-
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String txt_email = email.getText().toString().trim();
-                String txt_password = password.getText().toString().trim();
-
-                if(TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
-                    Toast.makeText(MainActivity.this, "Por Favor ingrese sus credenciales", Toast.LENGTH_SHORT).show();
-                }else{
-                    auth.signInWithEmailAndPassword(txt_email,txt_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                Intent intent = new Intent(MainActivity.this, InitActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                startActivity(intent);
-                                finish();
-                            }else{
-                                Toast.makeText(MainActivity.this,"Fallo la Autentificación", Toast.LENGTH_SHORT).show();
-                            }
-
-                        }
-                    });
-                }
-            }
-        });
-
-
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
-            }
-        });
     }
 }
